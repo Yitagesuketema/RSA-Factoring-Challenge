cc1ee3feba7a7cca3d9e78d1a74872aa5906e487
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-21 14:06:43 +0300

    command line argument added

--- factors.c
+++ factors.c
@@ -1,3 +1,22 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 1000
+
+int main(int argc, char *argv[])
+{
+if (argc == 2)
+    {
+    FILE *textfile;
+    char line[MAX_LINE_LENGTH];
+    textfile =fopen(argv[1], "r");
+    if(textfile == NULL)
+        return 1;
+            while(fgets(line, MAX_LINE_LENGTH, textfile))
+                {
+            printf(line);
+                }
+fclose(textfile);
+return 0;
+}
+}
