22cf244b292e792b09bd27ee9dd183c10014d143
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-22 11:32:33 +0300

    factors compiled

--- factors.c
+++ factors.c
@@ -1,23 +1,33 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_LINE_LENGTH 1000
-
 int main(int argc, char *argv[])
 {
 if (argc == 2)
     {
+         long int p,q,n;
+         int intofline;
     FILE *textfile;
-    int fact = 1;
     char line[MAX_LINE_LENGTH];
     textfile =fopen(argv[1], "r");
     if(textfile == NULL)
         return 1;
+
             while(fgets(line, MAX_LINE_LENGTH, textfile))
-                {
-            printf(line);
-                }
-fclose(textfile);
-return 0;
+{
+long long p, q, n = atoll(line);
+for ( p = 2;n%p !=0;p++)
+{
+
 }
+
+q =n/p;
+printf("%lld = %lld * %lld \n",n,q,p);
+
+}
+fclose(textfile);
+}
+return 0;
 }
--- main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H /* if MAIN_H not defined */
-#define MAIN_H  /* define MAIN_H */
-
-int is_prime (int n);
-
-#endif /* endof MAIN_H */
